d10bb2e480c56cc80b2e6976c36d441bc787e09e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2023-12-05 19:48:34 +0100

    Generate random grid

--- Class/grid.py
+++ Class/grid.py
@@ -4,24 +4,33 @@ from Class.box import Box
 
 
 class Grid():
+  """Grid class
+  This class represents the grid of a crossword puzzle.
+  """
   def __init__(self, rows, columns):
     self.rows = rows
     self.columns = columns
     self.grid = [[Box(row, col) for col in range(columns)] for row in range(rows)]
   
   def fill_grid_randomly(self):
+    """Fill the grid with random values"""
     for row in self.grid:
       for box in row:
-        if rd.random() < 0.2:
+        if rd.random() < 0.05:
           box.set_black()
+        elif rd.random() < 0.3:
+          box.set_letter(None)
         else:
           box.set_letter(chr(rd.randint(65, 90)))
 
-  def display(self):
+  def display_cli(self):
+    """Display the grid in the command line"""
     for row in self.grid:
       for box in row:
         if box.is_black:
-          print("X", end=" ")
+          print("■", end=" ")
         elif box.letter is not None:
           print(box.get_letter(), end=" ")
+        else:
+          print(" ", end=" ")
       print()
